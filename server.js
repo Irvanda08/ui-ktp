571307669d5ad3792f6638099693cc5bf4b3a228
Benedictus Irvanda Nugroho <irvandanugroho08@gmail.com>
2024-05-17 19:19:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.use(cors())
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, DELETE, PATCH"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
