d22be605476c0680cb80187365305645523fb5bb
Irvanda08 <irvandanugroho08@gmail.com>
2024-05-17 15:13:56 +0700

    p

---
+++ server.js
@@ -0,0 +1,33 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "SignUp.html"));
+});
+
+app.get("/edit", (req, res) => {
+  res.sendFile(path.join(__dirname, "edit.html"));
+});
+
+app.get("/input_data", (req, res) => {
+  res.sendFile(path.join(__dirname, "input_data.html"));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}...`);
+});
